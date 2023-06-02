e1e256afb972f542c0dbab72a387ab3145f678b0
Zive <wndk7720@naver.com>
2023-06-02 20:57:42 +0900

    change gpt model (text-davinci-003 -> gpt-3.5-turbo)

--- app/src/main/java/com/kakao_szbot/cmd/CommandGPT.java
+++ app/src/main/java/com/kakao_szbot/cmd/CommandGPT.java
@@ -6,7 +6,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Random;
 
 import okhttp3.MediaType;
@@ -33,7 +38,7 @@ public class CommandGPT {
         Log.d(TAG, "requestMsg: " + requestMsg);
 
         try {
-            return generateText(requestMsg, MAX_TOKEN);
+            return generateText(requestMsg, API_KEY);
         } catch (Exception e) {
             return "아쉽게 ChatGPT가 고장났답니다. 데헷☆";
         }
@@ -41,7 +46,7 @@ public class CommandGPT {
 
     public String gptDefaultMessage(String msg, String sender) {
         try {
-            return generateText(msg, MAX_TOKEN);
+            return generateText(msg, API_KEY);
         } catch (Exception e) {
             return "아쉽게 ChatGPT가 고장났답니다. 데헷☆";
         }
@@ -82,17 +87,72 @@ public class CommandGPT {
         return gptDefaultMessage(requestMsg, sender);
     }
 
+    private String generateText(String input, String apiKey) throws JSONException, IOException {
+        // Build input and API key params
+        String result = null;
+
+        JSONObject payload = new JSONObject();
+        JSONObject message = new JSONObject();
+        JSONArray messageList = new JSONArray();
+
+        message.put("role", "user");
+        message.put("content", input);
+        messageList.put(message);
+
+        payload.put("model", "gpt-3.5-turbo"); // model is important
+        payload.put("messages", messageList);
+        payload.put("temperature", 0.7);
+
+        URL url = new URL("https://api.openai.com/v1/chat/completions");
+        HttpURLConnection con = (HttpURLConnection)url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Authorization", "Bearer " + apiKey);
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setRequestProperty("Accept", "application/json");
+        con.setDoOutput(true);
+        con.setDoInput(true);
+
+        Log.d(TAG, "requestStr: " + payload.toString());
+        try(OutputStream os = con.getOutputStream()) {
+            byte[] inputByte = payload.toString().getBytes("utf-8");
+            os.write(inputByte, 0, inputByte.length);
+        }
+
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            result = response.toString();
+        }
+
+        JSONObject jsonResult = new JSONObject(result);
+        JSONArray jsonChoices = jsonResult.getJSONArray("choices");
+        JSONObject jsonMessage = jsonChoices.getJSONObject(0).getJSONObject("message");
+        result = jsonMessage.getString("content");
+        result = makePrettyText(result);
+        Log.d(TAG, "responseStr: " + result);
+
+        return result;
+    }
+
+    /*
     private String generateText(String prompt, int maxTokens) throws IOException, JSONException {
         OkHttpClient client = new OkHttpClient();
         MediaType mediaType = MediaType.parse("application/json");
         RequestBody body = RequestBody.create(mediaType, "{\"prompt\":\"" + prompt + "\",\"max_tokens\":" + maxTokens + ",\"temperature\":0.7}");
+        //RequestBody body = RequestBody.create(mediaType, "{\"model\": \"gpt-3.5-turbo\",\"messages\": [{\"role\":\"user\", \"content\":\"" + prompt + "\"}]");
         Request request = new Request.Builder()
                 //.url("https://api.openai.com/v1/engines/davinci-codex/completions")
                 .url("https://api.openai.com/v1/engines/text-davinci-003/completions")
+                //.url("https://api.openai.com/v1/chat/completions")
                 .post(body)
                 .addHeader("Content-Type", "application/json")
                 .addHeader("Authorization", "Bearer " + API_KEY)
                 .build();
+
         Response response = client.newCall(request).execute();
         String responseStr = response.body().string();
         Log.d(TAG, "responseStr: " + responseStr);
@@ -104,6 +164,7 @@ public class CommandGPT {
 
         return result;
     }
+    */
 
     private String makePrettyText(String result) {
         int start_index = result.indexOf("\n\n");
